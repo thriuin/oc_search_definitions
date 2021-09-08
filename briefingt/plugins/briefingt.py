7e3ec76903cea70c2baee15164bd725a4fce8abd
thriuin <ross.thompson@tbs-sct.gc.ca>
2021-09-08 12:58:17 -0400

    Briefing Note Titles Search

---
+++ briefingt/plugins/briefingt.py
@@ -0,0 +1,51 @@
+from django.http import HttpRequest
+from search.models import Search, Field, Code
+from SolrClient import SolrResponse
+
+
+def plugin_api_version():
+    return 1.0
+
+
+def pre_search_solr_query(context: dict, solr_query: dict, request: HttpRequest, search: Search, fields: dict, codes: dict, facets: list, record_ids: str):
+    return context, solr_query
+
+
+def post_search_solr_query(context: dict, solr_response: SolrResponse, solr_query: dict, request: HttpRequest, search: Search, fields: dict, codes: dict, facets: list, record_ids: str):
+    return context, solr_response
+
+
+def pre_record_solr_query(context: dict, solr_query: dict, request: HttpRequest, search: Search, fields: dict, codes: dict, facets: list, record_ids: str):
+    return context, solr_query
+
+
+def post_record_solr_query(context: dict, solr_response: SolrResponse, solr_query: dict, request: HttpRequest, search: Search, fields: dict, codes: dict, facets: list, record_ids: str):
+    return context, solr_response
+
+
+def pre_export_solr_query(solr_query: dict, request: HttpRequest, search: Search, fields: dict, codes: dict, facets: list):
+    return solr_query
+
+
+def post_export_solr_query(solr_response: SolrResponse, solr_query: dict, request: HttpRequest, search: Search, fields: dict, codes: dict, facets: list):
+    return solr_response
+
+
+def pre_mlt_solr_query(context: dict, solr_query: dict, request: HttpRequest, search: Search, fields: dict, codes: dict, record_is: str):
+    return context, solr_query
+
+
+def post_mlt_solr_query(context: dict, solr_response: SolrResponse, solr_query: dict, request: HttpRequest, search: Search, fields: dict, codes: dict, record_ids: str):
+    return context, solr_response
+
+
+def filter_csv_record(csv_record,search: Search, fields: dict, codes: dict, format: str):
+    return True,  csv_record
+
+
+def load_csv_record(csv_record: dict, solr_record: dict, search: Search, fields: dict, codes: dict, format: str):
+    if solr_record['title_en']:
+        solr_record['title_en'] = str(solr_record['title_en']).strip()
+    if solr_record['title_fr']:
+        solr_record['title_fr'] = str(solr_record['title_fr']).strip()
+    return solr_record
